027b913078bf82db19f4bc2e336eb2fe70241d37
Chloe Veras <chloe@chloes-air.lan>
2020-12-15 18:03:41 -0500

    Saving changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,16 +1,19 @@
-// from data.js
 var tableData = data;
-
-// YOUR CODE HERE!
-var button = d3.select("#filter-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "comments"];
-var filterDateTime = d3.select("#datetime");
 var tbody = d3.select("tbody"); 
 
-data.forEach((tableData) => {
+/* Appending rows to table */ 
+
+tableData.forEach((siting) => {
     var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
+    Object.entries(siting).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
+
+/* Filtering functionality begins here */ 
+
+var button = d3.select("#filter-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "comments"];
+var filterDateTime = d3.select("#datetime");
