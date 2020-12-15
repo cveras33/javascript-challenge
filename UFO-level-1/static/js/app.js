91604cc176148ff12f9a4a76da58bedb3444acee
Chloe Veras <chloe@chloes-air.lan>
2020-12-15 09:08:11 -0500

    Added rows to table - level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "comments"];
+var filterDateTime = d3.select("#datetime");
+var tbody = d3.select("tbody"); 
+
+data.forEach((tableData) => {
+    var row = tbody.append("tr");
+    Object.entries(tableData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
