cd9714fb739c88d460e953e1b2cd3ecbff4d8851
Chloe Veras <chloe@chloes-air.lan>
2020-12-16 09:21:45 -0500

    Completed level 1 filtering

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,11 @@
 var tableData = data;
 var tbody = d3.select("tbody"); 
+var columns = ["datetime", "city", "state", "country", "shape", "comments"];
 
+
+/***************************/
 /* Appending rows to table */ 
+/***************************/
 
 tableData.forEach((siting) => {
     var row = tbody.append("tr");
@@ -11,9 +15,55 @@ tableData.forEach((siting) => {
     });
   });
 
-
-/* Filtering functionality begins here */ 
+/********************************************/
+/* Form filtering functionality begins here */ 
+/********************************************/
 
 var button = d3.select("#filter-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "comments"];
-var filterDateTime = d3.select("#datetime");
+var form = d3.select("#form"); 
+
+// Creating Event Handler 
+button.on("click", onButtonClick);
+form.on("submit", onButtonClick);
+
+
+// Completing event handler function for the form 
+function onButtonClick() {
+  
+  d3.event.preventDefault(); 
+
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value").trim();
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+  console.log(filteredData);
+
+  tbody.html("");
+
+  let response = {
+    filteredData
+  }
+
+  if(response.filteredData.length !== 0)
+  {
+    filteredData.forEach((match) => {
+      var row = tbody.append("tr");
+      Object.entries(match).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+  }
+  else 
+  {
+    tbody.append("tr").append("td").text("No matches available.");
+  }
+}
+
+resetbtn.on("click", () => {
+  tbody.html("");
+  populate(data)
+  console.log("Table reset")
+}); 
